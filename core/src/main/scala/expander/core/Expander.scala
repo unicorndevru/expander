8bf3acc11471fa91ce6c88a29680508b46a2eb7a
Dmitry Kurinskiy <name.alari@gmail.com>
2016-03-23 00:18:38 +0300

    for nested arrays

--- core/src/main/scala/expander/core/Expander.scala
+++ core/src/main/scala/expander/core/Expander.scala
@@ -10,21 +10,49 @@ object Expander {
 
   val Key = "_expand"
 
-  def merge(l: JsObject, r: JsObject): JsObject = {
+  def mergeArr(l: JsArray, r: JsArray): JsArray =
+    JsArray(l.value zip r.value map {
+      case (ae: JsObject, ao: JsObject) ⇒
+        mergeObj(ae, ao)
+      case (ae: JsArray, ao: JsArray) ⇒
+        mergeArr(ae, ao)
+      case (_, ao) ⇒ ao
+    })
+
+  def mergeObj(l: JsObject, r: JsObject): JsObject = {
     JsObject((l.value ++ r.value).map {
       case (key, newValue) ⇒
         val maybeExistingValue = l.value.get(key)
         key → maybeExistingValue.fold(newValue)(ex ⇒ (ex, newValue) match {
-          case (e: JsObject, o: JsObject) ⇒ merge(e, o)
-          case (a: JsArray, o: JsArray) ⇒
-            JsArray(a.value zip o.value map {
-              case (ae: JsObject, ao: JsObject) ⇒
-                merge(ae, ao)
-              case (_, ao) ⇒ ao
-            })
-          case _ ⇒ newValue
+          case (e: JsObject, o: JsObject) ⇒ mergeObj(e, o)
+          case (a: JsArray, o: JsArray)   ⇒ mergeArr(a, o)
+          case _                          ⇒ newValue
         })
     })
+  }
+
+  def foldArr(v: JsValue): JsValue = v match {
+    case o: JsObject if o.value.nonEmpty && o.value.keys.forall(_.startsWith("$_")) ⇒
+      JsArray(o.value.map {
+        case (k, vv) ⇒ k.stripPrefix("$_").toInt → foldArr(vv)
+      }.toSeq.sortBy(_._1).map(_._2))
+    case o: JsObject ⇒
+      JsObject(o.value.mapValues(foldArr))
+    case a: JsArray ⇒
+      JsArray(a.value.map(foldArr))
+    case _ ⇒ v
+  }
+
+  def merge(l: JsObject, r: JsObject): JsObject = {
+    val obj = mergeObj(l, r)
+    JsObject(obj.value.mapValues(foldArr))
+  }
+
+  private def setInsideArray(tail: JsPath, v: JsValue): JsValue = if (tail.path.isEmpty) v else if (tail.path.forall(_.isInstanceOf[KeyPathNode])) tail.json.put(v).reads(Json.obj()).getOrElse(Json.obj()) else {
+    val safePath = JsPath(tail.path.takeWhile(p ⇒ !p.isInstanceOf[IdxPathNode]))
+    val subIdx = tail.path(safePath.path.length).asInstanceOf[IdxPathNode]
+    val unsafeTail = JsPath(tail.path.drop(safePath.path.length + 1))
+    safePath.json.put(Json.obj(s"$$_${subIdx.idx}" → setInsideArray(unsafeTail, v))).reads(Json.obj()).getOrElse(Json.obj())
   }
 
   def apply[T](root: T, reqs: PathRequest*)(implicit expandContext: ExpandContext[T], rootWrites: Writes[T]): Future[JsValue] = {
@@ -55,8 +83,7 @@ object Expander {
           case (kp, vs) ⇒
             val arrV = vs.collect {
               case (_, (IdxPathNode(idx), tail, v)) ⇒
-                println("tail = "+tail)
-                idx → (if (tail.path.isEmpty) v else tail.json.put(v).reads(Json.obj()).getOrElse(Json.obj()))
+                idx → setInsideArray(tail, v)
             }.sortBy(_._1).map(_._2)
             (kp, JsArray(arrV))
         }.toSeq
